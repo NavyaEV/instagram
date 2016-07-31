e5573a0528d3b64ae215b356a6eaf954055c35ed
NavyaEV <evnavya@gmail.com>
2016-07-31 23:38:16 +0530

    commit again

--- BasicInstagram/settings.py
+++ BasicInstagram/settings.py
@@ -136,3 +136,18 @@ TEMPLATE_DIR=(
 )
 import django.contrib.auth
 django.contrib.auth.LOGIN_URL = '/'
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn BasicInstagram.wsgi --log-file -
